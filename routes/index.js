aff394ffb8c815c2553391980286f4652222568d
Chayasorn Suansomjit <12016727+chayasorn@users.noreply.github.com>
2018-12-24 00:59:55 +0700

    Add sample JSON for render table. (Test Push JSON to Express via res.render())

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: 'Express',
+        uid: "39493000000001",
+        defaultKey : "FFFFFF39493000000001",
+        flagTamperTag : "AA",
+        flagTamperServer : "N/A",
+        flagTamperDecision : "N/A",
+        timeStampTag : "01234567",
+        timeStampServer : "N/A",
+        timeStampDecision : "N/A",
+        rollingCodeTag : "76543210",
+        rollingCodeServer : "87654321",
+        rollingCodeDecision : "Incorrect"
+    });
 });
 
 module.exports = router;
